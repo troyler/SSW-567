76f3a92934f2522a99bf3bec685c5b019d90f1cb
troyler <tmreinert22@gmail.com>
2022-11-16 19:46:58 -0500

    mocking different way

--- HW5a/mockTest.py
+++ HW5a/mockTest.py
@@ -1,6 +1,8 @@
 from unittest import mock
 import gHub
 import unittest
+import requests
+import json
 
 class Testing_gHub_API(unittest.TestCase):
 
@@ -17,4 +19,12 @@ class Testing_gHub_API(unittest.TestCase):
         mock_GUR.return_value = 6
         result = gHub.getUserRepos('troyler')
         self.assertEqual(result, 6)
+
+    @mock.patch('gHub.requests')
+    def testUserRepos(self, mock_GUR):
+        mock_response = mock.MagicMock()
+        mock_response.status_code = 200
+        f = open('hw4a/rk_repo.json')
+        mock_response.json.return_value = json.load(f)
+        f.close()
 
