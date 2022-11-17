309e41a9af82f752e45d931ce22a38d9b0259eb0
troyler <tmreinert22@gmail.com>
2022-11-16 19:50:21 -0500

    v

--- HW5a/mockTest.py
+++ HW5a/mockTest.py
@@ -21,7 +21,7 @@ class Testing_gHub_API(unittest.TestCase):
         self.assertEqual(result, 6)
 
     @mock.patch('gHub.requests')
-    def testUserRepos(self, mock_GUR):
+    def testUserRepos(self, mock_req):
         mock_response = mock.MagicMock()
         mock_response.status_code = 200
         f = open('hw4a/rk_repo.json')
